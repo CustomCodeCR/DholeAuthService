04362151cee65599edda7142f18090c65ab9cfae
Maurice Lang <106346768+MauLang18@users.noreply.github.com>
2026-09-21 09:00:09 -0600

    feat(auth): add impersonation scope constant

--- src/Dhole.Auth.Api/Authorization/AuthScopeNames.cs
+++ src/Dhole.Auth.Api/Authorization/AuthScopeNames.cs
@@ -10,6 +10,7 @@ internal static class AuthScopeNames
     public const string UsersSetLocked = "auth.users.set-locked";
     public const string UsersChangePassword = "auth.users.change-password";
     public const string UsersSendCredentials = "auth.users.send-credentials";
+    public const string UsersImpersonate = "auth.users.impersonate";
     public const string UsersRolesAssign = "auth.users.roles.assign";
     public const string UsersRolesRevoke = "auth.users.roles.revoke";
     public const string UsersScopesAssign = "auth.users.scopes.assign";
